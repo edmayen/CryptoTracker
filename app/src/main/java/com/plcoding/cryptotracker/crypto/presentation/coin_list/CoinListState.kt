191cbc5738c7aa2e1fc846b0587f9097bfae01d0
Eduardo Mayen <ed_mayen@hotmail.com>
2025-01-25 20:17:19 -0600

    wip: Handling network error

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListState.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListState.kt
@@ -7,6 +7,5 @@ import com.plcoding.cryptotracker.crypto.presentation.model.CoinUi
 data class CoinListState(
     val isLoading: Boolean = false,
     val coins: List<CoinUi> = emptyList(),
-    val selectedCoinUi: CoinUi? = null,
-    val error: String = ""
+    val selectedCoinUi: CoinUi? = null
 )
